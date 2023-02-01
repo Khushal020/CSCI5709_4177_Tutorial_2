d4c5c6e6f895926eacdb5b7551cd3f7f7739e8f1
Khushalkumar Maganbhai Gondaliya <khush@macbookpro.local>
2023-02-01 09:07:03 -0400

    tutorial 2 done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Basic app created by Khushalkumar Maganbhai Gondaliya - B00912092
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
